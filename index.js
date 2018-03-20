96f36a4eff4c200b15ba082f8c8bf723d7c1d8fb
LogannM <laug.fart20@gmail.com>
2018-03-20 09:47:22 +0100

    Final commit

--- index.js
+++ index.js
@@ -1,33 +1,99 @@
+#!/usr/bin/env node
 //spawn("ls *");
 
-const path = require('path');
-const fs = require('fs');
 const spawnCommand = require('spawn-command');
 const inquirer= require('inquirer')
 const Table = require('cli-table');
-const program = require('commander')
+const program = require('commander');
+const utils = require('./utils.js');
+const figlet = require('figlet');
 
-program.version('1.0.0')
-.option('-s, --AnnoyingFile', 'Start scan')
+
+console.log(figlet.textSync('AnnoyingFile Hunter !', {
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+}));
+
+let dirPath = './';
+let list = [];
+
+program.version('1.0.1')
+.option('-s, --AnnoyingFile', 'start scan')
+.option('-r, --recursive', 'recursive')
 
 program.parse(process.argv)
 
-if(program.AnnoyingFile)
-{
-	inquirer.prompt([
-	{
+if(program.AnnoyingFile) {
+	inquirer.prompt([{
 		type: 'list',
 		name: 'choices',
 		message: 'What do you want to do today ?',
 		choices: [
-		'1 : Scan the complete system (using tree)',
-		'2 : Scan one folder',
+		'Scan the complete system (using tree)',
+		'Scan current folder',
+		'Scan specific folder',
 		]
 	}])
 	.then((answers)=>{
 		console.log(answers)
-	});
+		if(answers.choices == 'Scan the complete system (using tree)') {
+			dirPath = ''
+		}
+		else if(answers.choices == 'Scan specific folder') {
+			inquirer.prompt([{
+				type: 'input',
+				name: 'dirPath',
+				message: 'Chemin du dossier ?\n'}]).then((anwser) => {
+					dirPath = anwser.dirPath;
+					console.log(dirPath);
+			});
+		}
+		const list = utils.filesInDir(dirPath);
+		/*console.log(list);*/
+	})
+	/*inquirer.prompt([{
+		type: 'checkbox',
+		name: 'choices',
+		message: 'Which one(s) do you want to delete ?',
+		choices: [
+			Afficher les fichiers les plus lourds à supprimer
+		]
+	}]).then(answers) => {
+		Suppression des fichiers sélectionnés
+	}*/
 }
+
+else {
+	program.help();
+}
+
+/*inquirer.prompt([
+{
+	type:'input',
+	message:'Entrez votre nom d\'utilisateur',
+	name:'username'
+},
+{
+	type:'password',
+	message:'Entrez votre mot de passe',
+	name:'password'
+},
+{
+	type:'checkbox',
+	message:'Que voulez-vous sauvegarder ?',
+	name:'foldersToSave',
+	choices: [
+	'Mes Documents',
+	'Mon bureau',
+	'Ma musique',
+	]
+}
+]).then((answers)=>{
+	console.log(answers)
+	})
+*/
+
+
 /*function getFileInfoFromFolder(route) {
 	const files = fs.readdirSync(route,'utf8');
 	const response = [];
